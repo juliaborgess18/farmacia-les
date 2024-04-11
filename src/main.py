c9d960454f8584152d66fae8393fab03faf33436
Julia Borges <juliaborgess18@gmail.com>
2024-04-10 22:41:45 -0300

    Desenvolvimento da tela de Cadastro de Cliente

--- src/main.py
+++ src/main.py
@@ -1,37 +1,24 @@
 ''' Arquivo principal para executar o programa'''
-
+import sys
 from model.dao.ConvenioDAO import ConvenioDAO
 from model.dao.DevolucaoDAO import DevolucaoDAO
 from model.database.BaseORM import BaseORM
 from sqlalchemy.orm import sessionmaker
 from model.domain.ItemDevolucao import ItemDevolucao
 from model.dao.ProdutoDAO import ProdutoDAO
-
-def printar_o_bom_garoto():
-    print('''
-         .--.             .---.
-        /:.  '.         .' ..  '._.---.
-       /:::-.  \.-"""-;` .-:::.     .::\\
-      /::'|  `\/  _ _  \\'   `\:'   ::::|
-  __.'    |   /  (o|o)  \     `'.   ':/
- /    .:. /   |   ___   |        '---'
-|    ::::'   /:  (._.) .:\\
-\    .='    |:'        :::|
- `""`       \     .-.   ':/
-             '---`|I|`---'
-                  '-'        
-
-          ''')
-    
-    print("au au...")
+from model.domain.Cliente import Cliente
+from model.dao.ClienteDAO import ClienteDAO
+import datetime
+from PyQt5 import QtCore, QtGui, QtWidgets
+from view.CadastroClienteController import Ui_Dialog
 
 def main():
-    print("Informação: Iniciando o Programa.")
-
-    print("Ainda não a nada aqui...")
-    printar_o_bom_garoto()
-     
-    print("Informação: Finalizando o Programa.")
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
--- src/model/dao/ClienteDAO.py
+++ src/model/dao/ClienteDAO.py
@@ -1,5 +1,6 @@
 from model.database.BaseORM import BaseORM
 from sqlalchemy.orm import sessionmaker
+from model.domain.Cliente import Cliente
 
 class ClienteDAO():
     engine = ''
@@ -9,3 +10,25 @@ class ClienteDAO():
         self.engine = BaseORM.get_engine()
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
+        
+    def select_all(self):
+        return self.session.query(Cliente).all()
+    
+    def select_by_id(self, id: int):
+        pass
+
+    def insert(self, cliente: Cliente):
+        try:
+            self.session.add(cliente)
+            self.session.commit()
+        except Exception as ex:
+            print(f"Error ao inserir o cliente: \n{ex}")
+            self.session.rollback()
+
+    def update(self, cliente: Cliente):
+        pass
+
+    def delete(self, cliente: Cliente):
+        pass
+        
+        
