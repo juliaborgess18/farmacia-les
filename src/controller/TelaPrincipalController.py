33b6a53f1fd3514a25f714a9bdbe1a8d660c7d19
Julia Borges <juliaborgess18@gmail.com>
2024-05-05 23:03:12 -0300

    Aplicação do CSS

--- src/controller/TelaPrincipalController.py
+++ src/controller/TelaPrincipalController.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QPushButton
 from controller.CadastroClienteController import TelaCliente
 from PyQt5.uic import loadUi
+from PyQt5.QtCore import QFile
     
 class openTelaPrincipal(QMainWindow):
     def __init__(self):
@@ -8,6 +9,7 @@ class openTelaPrincipal(QMainWindow):
         # Carregue a interface .ui
         loadUi('src/view/PrincipalView.ui', self)
         self.configureStackedWidget()
+        # self.loadStyleSheet('src/view/css/estilo.css')
     
     def configureStackedWidget(self):
         tela_vendas = TelaCliente()
@@ -25,4 +27,21 @@ class openTelaPrincipal(QMainWindow):
         self.stackedWidget.setCurrentIndex(2)
         
     def loadClientes(self):
+        self.btn_tela_clientes.setChecked(True)
         self.stackedWidget.setCurrentIndex(4)
+        
+    # def loadStyleSheet(self, file_name):
+    # # Carrega o arquivo .css
+    #     style_file = QFile(file_name)
+    #     if not style_file.open(QFile.ReadOnly | QFile.Text):
+    #         print("Erro ao abrir o arquivo de estilo:", style_file.errorString())
+    #         return
+    
+    #     # Lê o conteúdo do arquivo
+    #     style_sheet = style_file.readAll()
+    #     style_file.close()
+        
+    #     # Aplica o estilo à interface
+    #     self.btn_tela_clientes = False
+    #     if self.btn_tela_clientes.isChecked():
+    #         self.btn_tela_clientes.setStyleSheet(str(style_sheet, encoding='utf-8'))
