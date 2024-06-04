59f411016d39eef95b1b4e165dd1e707feb20b4c
Julia Borges <juliaborgess18@gmail.com>
2024-06-03 23:43:00 -0300

    update remover cliente e produto

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 import uvicorn
 from infrastructure.config.database import criar_bd
 from routes import produto_route
+from routes import cliente_route
 
 criar_bd()
 app = FastAPI()
@@ -12,14 +13,7 @@ templates = Jinja2Templates(directory="templates")
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 
 app.include_router(produto_route.router)
-
-@app.get("/", response_class=HTMLResponse)
-async def get_farmacia(request: Request):
-    return templates.TemplateResponse("index.html", {"request":request})
-
-@app.get("/cliente", response_class=HTMLResponse)
-async def get_cliente(request: Request):
-    return templates.TemplateResponse("/pages/clientes/cliente.html", { "request":request, "navItem": "Cliente", "urlItem": "cliente" })
+app.include_router(cliente_route.router)
 
 @app.get("/fornecedor", response_class=HTMLResponse)
 async def get_fornecedor(request: Request):
@@ -28,14 +22,6 @@ async def get_fornecedor(request: Request):
 @app.get("/venda", response_class=HTMLResponse)
 async def get_venda(request: Request):
     return templates.TemplateResponse("venda.html", {"request":request, "navItem": "Venda", "urlItem": "venda" })
-
-@app.get("/cadastrar_cliente", response_class=HTMLResponse)
-async def get_cadastro_cliente(request: Request):
-    return templates.TemplateResponse("/pages/clientes/cadastrar_cliente.html", {"request":request, "navItem": "Cliente", "urlItem": "cliente" })
-
-@app.get("/editar_cliente", response_class=HTMLResponse)
-async def get_atualiza_cliente(request: Request):
-    return templates.TemplateResponse("/pages/clientes/editar_cliente.html", {"request":request, "navItem": "Cliente", "urlItem": "cliente" })
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
--- src/routes/cliente_route.py
+++ src/routes/cliente_route.py
@@ -1,5 +1,31 @@
-
-# Utlize essa classe para todos os endpoins que tenham /cliente e similares
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+
+templates = Jinja2Templates(directory="templates")
+NAV_ITEM = "Cliente"
+URL_ITEM = "cliente"
+
+@router.get("/cliente", response_class=HTMLResponse)
+async def get_cliente(request: Request):
+    return templates.TemplateResponse("/pages/clientes/cliente.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM })
+
+@router.get("/cadastrar_cliente", response_class=HTMLResponse)
+async def get_cliente(request: Request):
+    return templates.TemplateResponse("/pages/clientes/cadastrar_cliente.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM })
+
+@router.get("/editar_cliente", response_class=HTMLResponse)
+async def get_editar_cliente(request: Request):
+    return templates.TemplateResponse("/pages/clientes/editar_cliente.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM })
+
+@router.get("/remover_cliente", response_class=HTMLResponse)
+async def get_remover_cliente(request: Request):
+    return templates.TemplateResponse("/pages/clientes/remover_cliente.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM })
+
+@router.get("/visualizar_cliente", response_class=HTMLResponse)
+async def get_visualizar_cliente(request: Request):
+    return templates.TemplateResponse("/pages/clientes/visualizar_cliente.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM })
+
+
