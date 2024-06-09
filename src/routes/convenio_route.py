5f0784531d254081ee97d7a236d2a3caf410a8ec
Igor <Igor.almeidadasilva13@gmail.com>
2024-06-09 01:01:30 -0300

    Implementation: Finalizando todas as telas do conveio

--- src/routes/convenio_route.py
+++ src/routes/convenio_route.py
@@ -21,6 +21,18 @@ async def get_cadastrar_convenio(request: Request):
     clientes = ClienteRepositorio.obter_todos()
     return templates.TemplateResponse("/pages/convenio/cadastrar_convenio.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM, "clientes": clientes})
 
+@router.get("/editar_convenio", response_class=HTMLResponse)
+async def get_editar_convenio(request: Request):
+    return templates.TemplateResponse("/pages/convenio/editar_convenio.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM})
+
+@router.get("/remover_convenio", response_class=HTMLResponse)
+async def get_remover_convenio(request: Request):
+    return templates.TemplateResponse("/pages/convenio/remover_convenio.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM})
+
+@router.get("/visualizar_convenio", response_class=HTMLResponse)
+async def get_visualizar_convenio(request: Request):
+    convenios = ConvenioRepositorio.obter_todos()
+    return templates.TemplateResponse("/pages/convenio/visualizar_convenio.html", {"request":request, "navItem": NAV_ITEM, "urlItem": URL_ITEM, "convenios": convenios})
 
 @router.post("/cadastrar_convenio")
 async def post_convenio(convenio: Convenio = Body()):
